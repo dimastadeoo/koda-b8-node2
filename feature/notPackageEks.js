a360283fdfd36d727bf9ae3cbede1e6c774f0ea4
dimastadeoo <dimastadeoo@gmail.com>
2026-05-27 12:45:57 +0700

    refactor: separate the convertTanggal function from index-manual.js to feature/notPackageEks.js

---
+++ feature/notPackageEks.js
@@ -0,0 +1,28 @@
+exports.convertTanggal = (isiTgl) => {
+  // buat dulu input menjadi array, dimana kita ambil datanya sebelum dipisah -
+  const partKal = isiTgl.split('-');
+
+  //cek jika panjang arraynya setelah dipisah kurang dari 3 maka format salah
+  if (partKal.length !== 3) {
+    console.log("Format tanggal DD-MM-YYYY");
+    return false;
+  }
+  // lalu masukkan isian 3 indeks tadi masing masing ke dalam variabel
+  const [day, month, year] = partKal.map(Number);
+
+  //Buat variabel tanggal dari objek Date menggunakan data year, month-1(karena pada object Date bulan januari adalah 0), day
+  const tanggal = new Date(year, month - 1, day);
+
+  // Verifikasi bahwa tahun, bulan, dan tanggal sesuai (mencegah 31-02-2015)
+  if (
+    tanggal.getDate() === day &&
+        tanggal.getMonth() === month - 1 &&
+        tanggal.getFullYear() === year
+  ) {
+    //merubah tanggal sesuai format penanggalan indonesia
+    return tanggal.toLocaleDateString("id-ID");
+  } else {
+    console.log("Format Tanggal Salah");
+    return false;
+  }
+};
--- index-manual.js
+++ index-manual.js
@@ -1,43 +1,18 @@
-const {createInterface} = require("node:readline");
-const rl = createInterface({
-  input : process.stdin,
-  output : process.stdout
-});
+const {convertTanggal} = require("./feature/notPackageEks.js");
+const {tanya, tutup} = require('./feature/inputData.js');
 
-function convertTanggal(isiTgl){
-  // buat dulu input menjadi array, dimana kita ambil datanya sebelum dipisah -
-  const partKal = isiTgl.split('-');
-
-  //cek jika panjang arraynya setelah dipisah kurang dari 3 maka format salah
-  if (partKal.length !== 3) {
-    console.log("Format tanggal DD-MM-YYYY");
-    isiTanggal();
-    return ;
+async function main(input, close) {
+  let inputData;
+  let result;
+  while(true){
+    inputData = await input("Inputkan tgl: ");
+    result = await convertTanggal(inputData);
+    if (result !== false){
+      break;
+    }
   }
-  // lalu masukkan isian 3 indeks tadi masing masing ke dalam variabel
-  const [day, month, year] = partKal.map(Number);
-
-  //Buat variabel tanggal dari objek Date menggunakan data year, month-1(karena pada object Date bulan januari adalah 0), day
-  const tanggal = new Date(year, month - 1, day);
-
-  // Verifikasi bahwa tahun, bulan, dan tanggal sesuai (mencegah 31-02-2015)
-  if (
-    tanggal.getDate() === day &&
-        tanggal.getMonth() === month - 1 &&
-        tanggal.getFullYear() === year
-  ) {
-    //merubah tanggal sesuai format penanggalan indonesia
-    console.log(tanggal.toLocaleDateString("id-ID"));
-    rl.close();
-  } else {
-    console.log("Format Tanggal Salah");
-    isiTanggal();
-    return;
-  }
+  console.log("Konversi tanggal: ", result);
+  close();
 }
-const isiTanggal = ()=>{
-  rl.question("Inputkan tgl: ", (isiTgl) => {
-    convertTanggal(isiTgl);
-  });
-};
-isiTanggal();
+
+main(tanya, tutup);
