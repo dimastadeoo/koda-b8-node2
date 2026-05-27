8f7c4e62bca3a33d29f6a568af17753361f980f5
dimastadeoo <dimastadeoo@gmail.com>
2026-05-27 12:48:02 +0700

    docs: add jsdocs in notPackageEks.js and index-manual.js

--- feature/notPackageEks.js
+++ feature/notPackageEks.js
@@ -1,3 +1,9 @@
+/**
+ * date conversion with internal package
+ * @param {string} isiTgl 
+ * @returns The value can be false if the input does not conform to the DD-MM-YYYY format or a date string in the DD/MM/YYYY format.
+ */
+
 exports.convertTanggal = (isiTgl) => {
   // buat dulu input menjadi array, dimana kita ambil datanya sebelum dipisah -
   const partKal = isiTgl.split('-');
