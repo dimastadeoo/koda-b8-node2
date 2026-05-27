d0e7ca428409cf7b76d994e694ae78e3429291f2
dimastadeoo <dimastadeoo@gmail.com>
2026-05-27 11:42:43 +0700

    feat: add user data input feature and handle readline closure in feature/inputData.js

---
+++ feature/inputData.js
@@ -0,0 +1,22 @@
+const {createInterface} = require("node:readline");
+const rl = createInterface({
+  input : process.stdin,
+  output : process.stdout
+});
+/**
+ * input Data
+ * @param {string} text a text in the form of a data input title
+ * @returns {string} returns a value from input data in the form of a string
+ */
+exports.tanya = (text)=>{
+  return new Promise((resolve)=>{
+    rl.question(text, (jawaban)=>{
+      resolve(jawaban);
+    });
+  });
+};
+
+exports.tutup = () => {
+  console.clear;
+  rl.close();
+};
